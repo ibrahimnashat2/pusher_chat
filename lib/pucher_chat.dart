35dddd4777994d38bd2755ba900649cc2dbdccfa
ibrahimnashat2 <ibrahimnashat@matnsolutions.com>
2023-03-07 15:38:39 +0200

    update

--- lib/pucher_chat.dart
+++ lib/pucher_chat.dart
@@ -5,14 +5,44 @@ import 'chatting-pusher/data/models/chat_room_model.dart';
 import 'core/injection/injectable.dart';
 import 'core/isar/isar.dart';
 
-Future<void> init() async {
-  configureDependencies();
-  final res = getIt<Isaar>();
-  await res.openObject(
-    schemas: [
-      ChatMessageModelSchema,
-      ChatRoomModelSchema,
-    ],
-    name: 'chatting_database',
-  );
+class PusherChat {
+  static late final Isaar _isaar;
+
+  static Future<void> init() async {
+    configureDependencies();
+    _isaar = getIt<Isaar>();
+    await _isaar.openObject(
+      schemas: [
+        ChatMessageModelSchema,
+        ChatRoomModelSchema,
+      ],
+      name: 'chatting_database',
+    );
+  }
+
+  @pragma('vm:entry-point')
+  static Future<void> saveRoomOrMessage(Map<String, dynamic> data) async {
+    if (data['user'] != null) {
+      _storeRoom(data);
+    }
+    if (data['message'] != null) {
+      _storeMessage(data);
+    }
+  }
+
+  @pragma('vm:entry-point')
+  static Future<void> _storeRoom(Map<String, dynamic> data) async {
+    await _isaar.writeOrUpdate<ChatRoomModel>(
+      object: ChatRoomModel.fromJson(data),
+    );
+  }
+
+  @pragma('vm:entry-point')
+  static Future<void> _storeMessage(Map<String, dynamic> data) async {
+    data["createdAt"] = DateTime.now().toIso8601String();
+    await _storeRoom(data);
+    await _isaar.writeOrUpdate<ChatMessageModel>(
+      object: ChatMessageModel.fromJson(data),
+    );
+  }
 }
