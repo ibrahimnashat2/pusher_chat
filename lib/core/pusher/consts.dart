1a6a867955ea988eb7f10cdf00b84db7b6cd085a
ibrahimnashat2 <ibrahimnashat@matnsolutions.com>
2023-03-13 10:56:39 +0200

    update

--- lib/core/pusher/consts.dart
+++ lib/core/pusher/consts.dart
@@ -1,9 +1,9 @@
 // ignore_for_file: constant_identifier_names, non_constant_identifier_names
 
 class PusherEnv {
-  static const PROJECT_ID = '5a8be8ba-c3e1-4e5d-bf27-caefac74ddea';
+  static const PROJECT_ID = '37d86909-c315-4927-95bc-06f60601c38d';
   static const BEARER_TOKEN =
-      '60D787480B00D14DEEE00463F7DFB97F2EBDD85BED8EB7D98B5EC86A3EF794EC';
+      '543747910FBD5CEBE599EC88604D4ACB55D6D478A582F59C370D5646D242A3AC';
   static const API_KEY = 'f821748907f137a77a2a';
   static const API_CLUSTER = 'eu';
   static const APP_ID = '1549449';
