f41e2bcaec6a496881c8b76c395d8c5c47990f0b
ibrahimnashat2 <ibrahimnashat@matnsolutions.com>
2023-03-14 11:51:21 +0200

    update

--- lib/pucher_chat.dart
+++ lib/pucher_chat.dart
@@ -1,17 +1,22 @@
 library pusher_chat;
 
+import 'chatting-pusher/data/data_sources/chat_local_data_source.dart';
 import 'chatting-pusher/data/models/chat_message_model.dart';
 import 'chatting-pusher/data/models/chat_room_model.dart';
 import 'chatting-pusher/data/models/history_model.dart';
+import 'chatting-pusher/domain/entities/chat_message.dart';
+import 'chatting-pusher/domain/entities/chat_user.dart';
 import 'core/injection/injectable.dart';
 import 'core/isar/isar.dart';
 
 class PusherChat {
   static late final Isaar _isaar;
 
+  static late final ChatLocalDataSource localDataSource;
   static Future<void> init() async {
     configureDependencies();
     _isaar = getIt<Isaar>();
+    localDataSource = getIt<ChatLocalDataSource>();
     await _isaar.openObject(
       schemas: [
         ChatMessageModelSchema,
@@ -22,29 +27,61 @@ class PusherChat {
     );
   }
 
-  @pragma('vm:entry-point')
-  static Future<void> saveRoomOrMessage(Map<String, dynamic> data) async {
-    if (data['user'] != null) {
-      _storeRoom(data);
-    }
-    if (data['message'] != null) {
-      _storeMessage(data);
-    }
+  static Future<void> onRecieveMessage({
+    required ChatMessageModel message,
+  }) async {
+    await _saveOrUpdateRoom(message);
+    await _saveMessage(message);
   }
 
-  @pragma('vm:entry-point')
-  static Future<void> _storeRoom(Map<String, dynamic> data) async {
-    await _isaar.writeOrUpdate<ChatRoomModel>(
-      object: ChatRoomModel.fromJson(data),
+  static Future<void> _saveOrUpdateRoom(ChatMessage message) async {
+    final rooms = await localDataSource.getAllRooms();
+    final index = rooms.indexWhere((item) => item.roomId == message.roomId);
+    int unReadCount = 0;
+    String senderName = '';
+    String lastUpdated = DateTime.now().toIso8601String();
+    ChatUser user = message.sender;
+    if (index != -1) {
+      ChatRoomModel room = rooms.elementAt(index);
+      final history = await localDataSource.getHistory();
+      final indx = history.indexWhere(
+        (item) => item.roomId == message.roomId && item.userId == user.id,
+      );
+      final iAmRepliedInRoom = indx != -1;
+
+      ///[if no customer service replied]
+      if ((room.lastCustomerService?.isEmpty ?? false) || iAmRepliedInRoom) {
+        unReadCount++;
+        unReadCount += room.unReadCount;
+      } else {
+        unReadCount = 0;
+      }
+      room.lastMessage = message.message;
+      room.lastUpdated = DateTime.now().toIso8601String();
+      senderName = "${room.lastCustomerService}";
+      rooms[index] = room;
+    }
+    if (message.reciever != null) {
+      user = message.reciever!;
+      senderName = message.sender.name;
+    }
+    await localDataSource.saveRoom(
+      lastMessage: message.message,
+      lastUpdated: lastUpdated,
+      roomId: message.roomId,
+      user: user,
+      lastCustomerService: senderName,
+      unReadCount: unReadCount,
     );
   }
 
-  @pragma('vm:entry-point')
-  static Future<void> _storeMessage(Map<String, dynamic> data) async {
-    data["createdAt"] = DateTime.now().toIso8601String();
-    await _storeRoom(data);
-    await _isaar.writeOrUpdate<ChatMessageModel>(
-      object: ChatMessageModel.fromJson(data),
+  static Future<void> _saveMessage(ChatMessageModel message) async {
+    await localDataSource.saveMessage(
+      message: message.message,
+      sender: message.sender,
+      messageId: message.messageId,
+      type: message.type,
+      roomId: message.roomId,
     );
   }
 
